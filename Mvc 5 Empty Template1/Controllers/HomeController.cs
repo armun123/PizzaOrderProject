21db98308a70cc0ab8a097bd59affcf7fa87a087
armun123 <hanna.kliushneva@gmail.com>
2016-10-26 22:57:07 -0500

    Version1.0
    Version 1.0

---
+++ Mvc 5 Empty Template1/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+
+namespace Mvc_5_Empty_Template1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Please Make An Order";
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string message)
+        {
+            ViewBag.Message = "Thank You For Your Order!";
+            return View();
+        }
+        //GET: Home/ordersPage
+        public ActionResult OrdersPage()
+
+        {
+
+            return View();
+
+        }
+    }
+}
