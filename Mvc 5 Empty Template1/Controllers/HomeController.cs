52afb9c5f705226a958330a086171bf944653ef7
armun123 <hanna.kliushneva@gmail.com>
2016-10-27 00:16:08 -0500

    Version1.0.2

--- Mvc 5 Empty Template1/Controllers/HomeController.cs
+++ Mvc 5 Empty Template1/Controllers/HomeController.cs
@@ -11,16 +11,53 @@ namespace Mvc_5_Empty_Template1.Controllers
     public class HomeController : Controller
     {
         // GET: Home
+        PizzaDBEntities1 _db;
+
+        public HomeController()
+        {
+            _db = new PizzaDBEntities1();
+        }
+
         public ActionResult Index()
         {
             ViewBag.Message = "Please Make An Order";
             return View();
         }
         [HttpPost]
-        public ActionResult Index(string message)
+        public ActionResult Index(FormCollection form)
         {
+            ViewData.Model = _db.tbl_orders.ToList();
             ViewBag.Message = "Thank You For Your Order!";
             return View();
+        }
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Add(FormCollection form)
+        {
+            var orderToAdd = new tbl_orders();
+
+            // Deserialize (Include white list!)
+            TryUpdateModel(orderToAdd, new string[] { "Date", "Count" }, form.ToValueProvider());
+
+            // Validate
+            if (String.IsNullOrEmpty(orderToAdd.deliveredDate.ToString()))
+                ModelState.AddModelError("Delivery Date", "DeliveryDate is required!");
+            if (String.IsNullOrEmpty(orderToAdd.countPZ.ToString()))
+                ModelState.AddModelError("Count", "Count of Pizzas is required!");
+
+            // If valid, save movie to database
+            if (ModelState.IsValid)
+            {
+                //_db.AddToOrderSet(orderToAdd);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            // Otherwise, reshow form
+            return View(orderToAdd);
         }
         //GET: Home/ordersPage
         public ActionResult OrdersPage()
---
+++ Mvc 5 Empty Template1/Models/order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Mvc_5_Empty_Template1.Models
+{
+    public class order
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string phone { get; set; }
+        public string userEmail { get; set; }
+        [DataType(DataType.Date)]
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public System.DateTime deliveredDate { get; set; }
+        public int countPZ { get; set; }
+    }
+}
